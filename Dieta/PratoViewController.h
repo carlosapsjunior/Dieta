6f32aa16b6d1496e1bf9ff023b60ea7cbe35d61b
Carlos Augusto Pereira da Silva Junior <carloss591@gmail.com>
2014-09-15 17:45:12 -0300

    Anotacoes

--- Dieta/PratoViewController.h
+++ Dieta/PratoViewController.h
@@ -36,4 +36,26 @@
 - (IBAction)mostraConfirmacaoParaLimparPrato:(id)sender;
 - (IBAction)processaAlimentos:(id)sender;
 
+//Porções: 19
+
+//Café da manha
+//- Um alimento do grupo dos carboidratos: biscoito, torrada, algum tipo de pão ou cereal matinal.
+//- Um alimento do grupo das frutas: frutas frescas, vitaminas ou sucos de frutas.
+//- Um alimento do grupo dos leites e derivados: leite, iogurte ou queijo.
+
+//Almoço e Jantar
+//- Verduras: 2 ou mais.
+//- Legumes
+//- Fonte de carboidratos: Arroz e macarrão, batata, farinhas, farofas e purê de batata.
+//- Grãos: feijões, grão de bico, lentilha, soja.
+//- Carnes: aves, peixes e carnes magras
+
+//Lanches
+//- Frutas
+//- Iogurtes
+//- Biscoitos e cookies integrais:
+//- Carboidratos integrais:
+//
+//
+
 @end
