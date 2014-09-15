af7abf35a01244a2aaf660c748361b27a5e9ab1e
danielteodoro <daniel_missima@hotmail.com>
2014-09-15 15:04:22 -0300

    Telas Jogos/Skate

---
+++ Dieta/SkateViewController.h
@@ -0,0 +1,13 @@
+//
+//  SkateViewController.h
+//  Dieta
+//
+//  Created by Daniel Missima Teodoro on 11/09/14.
+//  Copyright (c) 2014 senac2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkateViewController : UIViewController
+
+@end
